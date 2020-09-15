3a2ac44cd450868fef2bc0a86967e272d0a127e8
dpSky <56128195+dpSky@users.noreply.github.com>
2020-09-15 09:24:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,41 +10,42 @@ else
     apt-get install unzip wget curl -y > /dev/null
     apt-get update curl -y
 fi
+timedatectl set-timezone Asia/Shanghai
 
 api=$1
 key=$2
 nodeId=$3
 localPort=$4
 license=$5
-folder=$key-trojan
+folder=$key-tj
 if [[ "$6" -ne "" ]]
     then
     syncInterval=$6
 else
     syncInterval=60
 fi
-
-#kill old process and delete dir
-systemctl stop firewalld
-systemctl disable firewalld
-systemctl stop trojan-go.service
-systemctl stop vvlink.service
-systemctl stop vvlink-tj.service
+#kill process and delete dir
 kill -9 $(ps -ef | grep ${folder} | grep -v grep | grep -v bash | awk '{print $2}') 1 > /dev/null
 kill -9 $(ps -ef | grep defunct | grep -v grep | awk '{print $2}') 1 > /dev/null
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl stop vvlink-tj.service
+systemctl disable vvlink-tj.service
 echo '结束进程'
+rm -rf $key
 rm -rf $folder
-echo '结束进程'
-sleep 3
-rm -rf /etc/systemd/system/trojan-go.service
-rm -rf /etc/systemd/system/vvlink.service
-rm -rf /etc/systemd/system/vvlink-tj.service
-rm -rf $key-trojan
 rm -rf $license
 
 #create dir, init files
-mkdir $license
-cd $license
+mkdir $folder
+cd $folder
+#create ssl ceart
+wget https://dpsky.cn/vvlink-a07wm6/vvlink.key
+wget https://dpsky.cn/vvlink-a07wm6/vvlink.crt
+mkdir /root/.cert
+cp vvlink.crt /root/.cert/server.crt
+cp vvlink.key /root/.cert/server.key
+chmod 400 /root/.cert/server.*
 wget https://github.com/tokumeikoi/tidalab-trojan/releases/latest/download/tidalab-trojan
 wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.8.1/trojan-go-linux-amd64.zip
 curl "${api}/api/v1/server/TrojanTidalab/config?token=${key}&node_id=${nodeId}&local_port=${localPort}" > ./config.json
@@ -56,13 +57,6 @@ else
     echo '配置获取失败'
     exit
 fi
-
-wget https://dpsky.cn/vvlink-a07wm6/vvlink.key
-wget https://dpsky.cn/vvlink-a07wm6/vvlink.crt
-mkdir /root/.cert
-cp vvlink.crt /root/.cert/server.crt
-cp vvlink.key /root/.cert/server.key
-chmod 400 /root/.cert/server.*
 
 unzip trojan-go-linux-amd64.zip
 chmod 755 *
@@ -77,6 +71,8 @@ fi
 
 #run server
 nohup `pwd`/tidalab-trojan -api=$api -token=$key -node=$nodeId -localport=$localPort -license=$license -syncInterval=$syncInterval > tidalab.log 2>&1 &
+echo '启动成功'
+sleep 3
 cat tidalab.log
 if ls | grep "service.log"
 	then
@@ -85,24 +81,25 @@ else
 	echo '启动失败'
 fi
 
+#create auto start
 cat << EOF >> /etc/systemd/system/vvlink-tj.service
 [Unit]
 Description=vvLink-tj Service
 After=network.target
 Wants=network.target
-
 [Service]
+
 Type=simple
 PIDFile=/run/vvlink-tj.pid
-ExecStart=/root/$license/tidalab-trojan -api=$api -token=$key -node=$nodeId -localport=$localPort -license=$license -syncInterval=$syncInterval
+ExecStart=/root/$folder/tidalab-trojan -api=$api -token=$key -node=$nodeId -localport=$localPort -license=$license -syncInterval=$syncInterval > tidalab.log 2>&1 &
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl daemon-reload
+
 systemctl enable vvlink-tj
-systemctl start vvlink-tj
+systemctl daemon-reload
 echo '部署完成'
 sleep 3
 systemctl status vvlink-tj
